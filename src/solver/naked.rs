90c49662a06ad05a62e2ec86830f3d1d2ef13001
KRODU <nscksp@gmail.com>
2024-02-11 16:57:48 +0900

    naked 멀티스레딩 변경

--- src/solver/naked.rs
+++ src/solver/naked.rs
@@ -38,26 +38,23 @@ impl<'a, const N: usize> Solver<'a, N> {
             }
 
             s.spawn_fifo(|_| {
-                if let Some(result) = self.naked_number_zone(zone, cells, read, is_break) {
-                    let mut result_list_lock = result_list.lock().unwrap();
-                    result_list_lock.push(result);
-                }
+                self.naked_number_zone(zone, cells, read, result_list, is_break);
             });
         }
     }
 
     #[inline]
-    fn naked_number_zone(
+    fn naked_number_zone<'b>(
         &self,
         zone: &Zone,
-        cells: &Vec<&'a Cell<N>>,
-        read: &TableLockReadGuard<N>,
-        is_break: &NonAtomicBool,
+        cells: &'b Vec<&'a Cell<N>>,
+        read: &'b TableLockReadGuard<N>,
+        result_list: &'b Mutex<Vec<SolverResult<'a, N>>>,
+        is_break: &'b NonAtomicBool,
     ) -> Option<SolverResult<'a, N>> {
         if is_break.get() {
             return None;
         }
-
         let non_final_cells = {
             let mut non_final_cells: Vec<&Cell<N>> = Vec::with_capacity(cells.len());
             non_final_cells.extend(
@@ -68,89 +65,100 @@ impl<'a, const N: usize> Solver<'a, N> {
             );
             non_final_cells
         };
-        let mut comp_cell_target: Vec<&Cell<N>> = Vec::with_capacity(non_final_cells.len());
 
-        for i in 2..N / 2 {
-            comp_cell_target.clear();
-            // 검증대상 cell 필터링 후 처리. 이렇게 하면 처리 시간을 많이 줄일 수 있음.
-            comp_cell_target.extend(
-                non_final_cells
-                    .iter()
-                    .filter(|c| read.read_from_cell(c).true_cnt() <= i),
-            );
+        let non_final_cells = &non_final_cells;
 
-            let mut comb_iter = Combination::new(&comp_cell_target, i);
+        rayon::scope_fifo(|s| {
+            for i in 2..N / 2 {
+                s.spawn_fifo(move |_| {
+                    let mut comp_cell_target: Vec<&Cell<N>> =
+                        Vec::with_capacity(non_final_cells.len());
+                    // 검증대상 cell 필터링 후 처리. 이렇게 하면 처리 시간을 많이 줄일 수 있음.
+                    comp_cell_target.extend(
+                        non_final_cells
+                            .iter()
+                            .filter(|c| read.read_from_cell(c).true_cnt() <= i),
+                    );
 
-            'comb_loop: while let Some(arr) = comb_iter.next_comb() {
-                if is_break.get() {
-                    return None;
-                }
+                    let mut comb_iter = Combination::new(&comp_cell_target, i);
 
-                debug_assert_eq!(i, arr.len());
-                let mut union_bit_flag = u64::MIN;
-                for c in arr {
-                    let b = read.read_from_cell(c);
-                    union_bit_flag |= b.bit_flag();
+                    'comb_loop: while let Some(arr) = comb_iter.next_comb() {
+                        if is_break.get() {
+                            return;
+                        }
 
-                    if union_bit_flag.count_ones() > i as u32 {
-                        continue 'comb_loop;
-                    }
-                }
+                        debug_assert_eq!(i, arr.len());
+                        let mut union_bit_flag = u64::MIN;
+                        for c in arr {
+                            let b = read.read_from_cell(c);
+                            union_bit_flag |= b.bit_flag();
 
-                let union_node_true_cnt = union_bit_flag.count_ones() as usize;
+                            if union_bit_flag.count_ones() > i as u32 {
+                                continue 'comb_loop;
+                            }
+                        }
 
-                if union_node_true_cnt != i {
-                    continue 'comb_loop;
-                }
+                        let union_node_true_cnt = union_bit_flag.count_ones() as usize;
 
-                let mut effect_cells: Vec<(&Cell<N>, ArrayVector<MaxNum<N>, N>)> = Vec::new();
-                // zone을 순회하며 삭제할 노트가 있는지 찾음
-                for zone_cell in cells {
-                    // 순회 대상에서 자기 자신은 제외
-                    // combinations 함수는 입력 배열이 정렬되어있을 경우 출력 배열 또한 정렬되어 있음
-                    // cells -> comp_cell_target -> combinations 함수로 데이터가 흘러가며 cells가 정렬되어있으니 combinations또한 정렬됨
-                    if arr.binary_search(&zone_cell).is_ok() {
-                        continue;
-                    }
-                    debug_assert!(!arr.contains(&zone_cell)); // 실수로 버그를 도입할 수 있으니 이중체크..
+                        if union_node_true_cnt != i {
+                            continue 'comb_loop;
+                        }
 
-                    let b = read.read_from_cell(zone_cell);
-                    let mut inter: ArrayVector<MaxNum<N>, N> = ArrayVector::new();
-                    for &true_note in b.get_true_list() {
-                        if union_bit_flag & (1 << true_note.get_value()) != 0 {
-                            inter.push(true_note);
+                        let mut effect_cells: Vec<(&Cell<N>, ArrayVector<MaxNum<N>, N>)> =
+                            Vec::new();
+                        // zone을 순회하며 삭제할 노트가 있는지 찾음
+                        for zone_cell in cells {
+                            // 순회 대상에서 자기 자신은 제외
+                            // combinations 함수는 입력 배열이 정렬되어있을 경우 출력 배열 또한 정렬되어 있음
+                            // cells -> comp_cell_target -> combinations 함수로 데이터가 흘러가며 cells가 정렬되어있으니 combinations또한 정렬됨
+                            if arr.binary_search(&zone_cell).is_ok() {
+                                continue;
+                            }
+                            debug_assert!(!arr.contains(&zone_cell)); // 실수로 버그를 도입할 수 있으니 이중체크..
+
+                            let b = read.read_from_cell(zone_cell);
+                            let mut inter: ArrayVector<MaxNum<N>, N> = ArrayVector::new();
+                            for &true_note in b.get_true_list() {
+                                if union_bit_flag & (1 << true_note.get_value()) != 0 {
+                                    inter.push(true_note);
+                                }
+                            }
+
+                            // 제거할 노트를 발견한 경우
+                            if !inter.is_empty() {
+                                is_break.set(true);
+                                if effect_cells.is_empty() {
+                                    effect_cells.reserve_exact(N);
+                                }
+                                effect_cells.push((zone_cell, inter));
+                            }
+                        }
+
+                        // effect_cells에 값이 존재하는 경우 제거한 노트를 발견한 것임.
+                        if !effect_cells.is_empty() {
+                            let mut found_chks: ArrayVector<MaxNum<N>, N> = ArrayVector::new();
+                            for n in MaxNum::<N>::iter() {
+                                if union_bit_flag & (1 << n.get_value()) != 0 {
+                                    found_chks.push(n);
+                                }
+                            }
+                            debug_assert_eq!(found_chks.len(), union_node_true_cnt);
+                            let result = SolverResult {
+                                solver_type: SolverResultDetail::Naked {
+                                    found_chks,
+                                    found_cell: arr.iter().map(|c| **c).collect(),
+                                },
+                                effect_cells,
+                            };
+
+                            let mut result_list_lock = result_list.lock().unwrap();
+                            result_list_lock.push(result);
+                            return;
                         }
                     }
-
-                    // 제거할 노트를 발견한 경우
-                    if !inter.is_empty() {
-                        is_break.set(true);
-                        if effect_cells.is_empty() {
-                            effect_cells.reserve_exact(N);
-                        }
-                        effect_cells.push((zone_cell, inter));
-                    }
-                }
-
-                // effect_cells에 값이 존재하는 경우 제거한 노트를 발견한 것임.
-                if !effect_cells.is_empty() {
-                    let mut found_chks: ArrayVector<MaxNum<N>, N> = ArrayVector::new();
-                    for n in MaxNum::<N>::iter() {
-                        if union_bit_flag & (1 << n.get_value()) != 0 {
-                            found_chks.push(n);
-                        }
-                    }
-                    debug_assert_eq!(found_chks.len(), union_node_true_cnt);
-                    return Some(SolverResult {
-                        solver_type: SolverResultDetail::Naked {
-                            found_chks,
-                            found_cell: arr.iter().map(|c| **c).collect(),
-                        },
-                        effect_cells,
-                    });
-                }
+                });
             }
-        }
+        });
 
         self.zone_cache
             .checked_zone_set_bool_true(*zone, SolverSimple::Naked);
